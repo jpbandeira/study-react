6ad60d27aa618464bbfae2f64d650f7317342ad4
pedrobandeira <pedrobandeira@vialogicasistemas.com.br>
2019-07-03 10:24:11 -0300

    Concluindo curso de react, com criação da tela de exibição de informações dos produtos

--- study/src/Products/products.js
+++ study/src/Products/products.js
@@ -1,7 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import api from "../Services/api";
+import './style-products.css';
 
 export default class Product extends Component{
+    state = {
+        product: {},
+    };
+
+    async componentDidMount(){
+        const { id } = this.props.match.params;
+
+        const response = await api.get(`/products/${id}`);
+
+        this.setState({ product: response.data });
+    }
+    
     render(){
-        return <h1>Detalhes produtos</h1>
+        const { product } = this.state;
+        console.log(product.title)
+        return(
+            <div className="product-info">
+                <h1>{product.title}</h1>
+                <p>{product.description}</p>
+
+                <a href={product.url}>URL: {product.url}</a>
+            </div>            
+        );
     }
 };
